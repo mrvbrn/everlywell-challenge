e9a93c0ce003ea6b5107677fe3aeb19c36acc6e5
Merve Baran <mrvbrn@Merves-MacBook-Air.local>
2021-07-24 12:55:37 -0700

    added a link as a source of meal  and route another tab

--- src/App.js
+++ src/App.js
@@ -4,26 +4,29 @@ import './App.css';
 
 const App = () => {
   const [mealData, setMealData] = useState([])
+
   const getMealData = useCallback(async() => {
     try{
       const response = await axios('https://www.themealdb.com/api/json/v1/1/random.php')
-      setMealData((prevData) => [...prevData, response.data.meals])
+      if(!mealData.includes(response.data.meals)){
+         setMealData((prevData) => [...prevData, response.data.meals])
+      }  
     }catch(err){
       console.log(err)
     }
   },[mealData])
   console.log(mealData)
   useEffect(() => {
-    for(let i=0; i<5; i++){
+    if(mealData.length < 5){
       getMealData()
-    } 
-  },[])
+    }
+  },[mealData])
   return (
     <div className="App">
       {mealData && mealData.map((meal) => {
         return(
           <ul key={meal[0].idMeal}>
-          <li><a href={meal[0].strSource}></a></li>
+          <li><a href={meal[0].strSource ? meal[0].strSource : meal[0].strYoutube } target="_blank">{meal[0].strSource ? meal[0].strSource : meal[0].strYoutube}</a></li>
           </ul>
         )
       })}
