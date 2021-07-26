fe8cb7465218291a48a2f72c8990536411148c95
Merve Baran <mrvbrn@Merves-MacBook-Air.local>
2021-07-25 22:17:53 -0700

    added router && when click meal route recipe page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect, useCallback } from "react";
 import axios from 'axios'; 
+import { Link, Route
+} from "react-router-dom";
+
 import './App.css';
+import Recipes from "./Recipes";
+
 
 const App = () => {
   const [mealData, setMealData] = useState([])
@@ -8,25 +13,30 @@ const App = () => {
   const getMealData = useCallback(async() => {
     try{
       const response = await axios('https://www.themealdb.com/api/json/v1/1/random.php')
-      if(!mealData.includes(response.data.meals)){
-         setMealData((prevData) => [...prevData, response.data.meals])
-      }  
+      const filtereData = mealData.filter(mdata => mdata.idMeal !== response.data.meals[0].idMeal)
+      if(filtereData){
+        setMealData((prevData) => [...prevData, response.data.meals]) 
+      }
+    
     }catch(err){
       console.log(err)
     }
   },[mealData])
-  console.log(mealData)
+
   useEffect(() => {
     if(mealData.length < 5){
       getMealData()
     }
   },[mealData])
+
   return (
     <div className="App">
       {mealData && mealData.map((meal) => {
         return(
           <ul key={meal[0].idMeal}>
-          <li><a href={meal[0].strSource ? meal[0].strSource : meal[0].strYoutube } target="_blank">{meal[0].strSource ? meal[0].strSource : meal[0].strYoutube}</a></li>
+            <li>
+              <Link to={`mealName=${meal[0].strMeal}`}>{meal[0].strMeal}</Link>
+            </li>
           </ul>
         )
       })}
---
+++ src/RouterPage.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Recipes from "./Recipes";
+import App from "./App";
+
+
+const RouterPage = () => {
+  return (
+    <Router>
+      <div>
+          <Route exact path="/" component={App} />
+          <Route path="/mealName:mealName" component={Recipes} />
+      </div>
+    </Router>
+  )
+}
+
+
+export default RouterPage;
